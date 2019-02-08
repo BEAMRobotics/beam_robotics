0df2fa9f7c23e4a569b0029985b0523afd7e6125
emmclaugh <emmclaugh@uwaterloo.ca>
2019-02-08 15:55:32 -0500

    fixing novatel submodule

--- novatel_span_driver/novatel_span_driver/setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-
-setup(**generate_distutils_setup(
-    packages=['novatel_span_driver'],
-    package_dir={'': 'src'}
-))
