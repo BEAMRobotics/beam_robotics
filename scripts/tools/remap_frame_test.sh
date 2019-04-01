2613e29a6ece8ad69db60ab7ed4f2a86abcee548
sh2fong <46571972+sh2fong@users.noreply.github.com>
2019-04-01 10:39:01 -0400

    Updated name of python script that is called by shell script

--- scripts/tools/remap_frame_test.sh
+++ scripts/tools/remap_frame_test.sh
@@ -2,12 +2,12 @@
 
 set -e # exit on first error
 
-# Test cases for path coverage of 'remap_frame_v2.py'
+# Test cases for path coverage of 'remap_frames.py'
 # The following shell script iteratively calls 'remap_frame_v2.py' with different raw input arguments to check for bugs on all paths
 
 # Case 1 - Single valid topic/rename frame with valid input
 
-python ./remap_frames_v2_test.py <<EOF
+python ./remap_frames_test.py <<EOF
 /home/inspector-gadget/bag_files/ig_scans/ig_scan_2019-02-14-12-39-13_short.bag
 /home/inspector-gadget/bag_files/ig_scans/ig_scan_2019-02-14-12-39-13_short_A.bag
 /hvlp/velodyne_points
@@ -17,7 +17,7 @@ echo "Case 1 complete: Single valid topic with valid rename"
 
 # Case 2 - Single invalid topic
 
-python ./remap_frames_v2_test.py <<EOF
+python ./remap_frames_test.py <<EOF
 /home/inspector-gadget/bag_files/ig_scans/ig_scan_2019-02-14-12-39-13_short.bag
 /home/inspector-gadget/bag_files/ig_scans/ig_scan_2019-02-14-12-39-13_short_A.bag
 /hvlp/invalid_topic1+?
@@ -27,7 +27,7 @@ echo "Case 2 complete: Single invalid topic"
 
 # Case 3 - Multiple (2) valid topics/rename frames with valid inputs
 
-python ./remap_frames_v2_test.py <<EOF
+python ./remap_frames_test.py <<EOF
 /home/inspector-gadget/bag_files/ig_scans/ig_scan_2019-02-14-12-39-13_short.bag
 /home/inspector-gadget/bag_files/ig_scans/ig_scan_2019-02-14-12-39-13_short_A.bag
 /hvlp/velodyne_points,/vvlp/velodyne_points
@@ -38,7 +38,7 @@ echo "Case 3 complete: Multiple (2) valid topics with valid renames"
 
 # Case 4 - Multiple topics (2) (1) valid (1) invalid
 
-python ./remap_frames_v2_test.py <<EOF
+python ./remap_frames_test.py <<EOF
 /home/inspector-gadget/bag_files/ig_scans/ig_scan_2019-02-14-12-39-13_short.bag
 /home/inspector-gadget/bag_files/ig_scans/ig_scan_2019-02-14-12-39-13_short_A.bag
 /hvlp/velodyne_points,/vvlp/invalid_topic1+?
