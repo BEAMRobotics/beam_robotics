1f41be2349e07483b718ddaf86411b22c1f80b76
Avery Jung <yhj.ser@gmail.com>
2019-06-10 13:07:24 -0400

    Calibration publisher package * Load extrinsics calibration json and publish them to tf_static topic

--- calibration/calibration_publisher/src/calibration_publisher_node.cpp
+++ calibration/calibration_publisher/src/calibration_publisher_node.cpp
@@ -8,29 +8,44 @@ int main(int argc, char* argv[]) {
 
   ros::NodeHandle node_handle("~");
 
+  static tf2_ros::StaticTransformBroadcaster static_broadcaster;
+
   std::string robot_name;
   if (!node_handle.getParam("robot_name", robot_name)) {
     ROS_ERROR("Failed to load parameter");
   }
-  ROS_INFO("Finished loading parameters");
 
-  std::string current_filepath = "calibration_util/src/calibration_publisher_node.cpp";
+  ROS_INFO("Finished loading parameters");
+  robot_name = "inspector_gadget";
+
+  std::string current_filepath = "calibration_publisher/src/calibration_publisher_node.cpp";
 
   std::string calibration_filepath = __FILE__;
+  std::cout << calibration_filepath << std::endl;
   calibration_filepath.erase(calibration_filepath.end() -
                                  current_filepath.size(),
                              calibration_filepath.end());
-  calibration_filepath += "results";
+  calibration_filepath += "results/";
   calibration_filepath += robot_name;
   calibration_filepath += "/current/extrinsics/extrinsics.json";
+  std::cout << calibration_filepath << std::endl;
 
   beam_calibration::TfTree tf_tree;
 
   tf_tree.LoadJSON(calibration_filepath);
 
-  std::cout << tf_tree.GetAllFrames() << std::endl;
+  auto frames = tf_tree.GetAllFrames();
+  std::string parent_frame;
+  for (auto frame : frames) {
+    parent_frame = frame.first;
+    for (auto child_frame : frame.second) {
+      auto tf_msg = tf_tree.GetTransform(parent_frame, child_frame, ros::Time::now());
+      static_broadcaster.sendTransform(tf_msg);
+    }
+  }
 
   ROS_INFO("Successfully launched node.");
+  ros::spin();
 
   return 0;
 }
