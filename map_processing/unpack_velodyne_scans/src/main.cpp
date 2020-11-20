0a47ce0db5e3c1ce9d277da8ed9abfa8f9092619
Alexander <adthoms@uwaterloo.ca>
2020-11-20 12:00:11 -0500

    clang formatting]

--- map_processing/unpack_velodyne_scans/src/main.cpp
+++ map_processing/unpack_velodyne_scans/src/main.cpp
@@ -4,22 +4,22 @@
 int main(int argc, char* argv[]) {
 	if (argc != 3) {
     std::cerr << "----------------------------------------------------------\n"
-    		<< "**USAGE SUMMARY**\n"
-            << "Enter the full file path to bag file as well as the velodyne"
-			<< "calibration file as follows: \n"
-            << "USAGE: " << argv[0] << " /path/to/bag/file.bag"
-			<< "velodyne_calibration.yaml \n"
-			<< "Note: Beam currently uses VLP16_hires_db.yaml \n"
-            << "----------------------------------------------------------\n";
+		<< "**USAGE SUMMARY**\n"
+		<< "Enter the full file path to bag file as well as the velodyne"
+		<< "calibration file as follows: \n"
+		<< "USAGE: " << argv[0] << " /path/to/bag/file.bag"
+		<< "velodyne_calibration.yaml \n"
+		<< "Note: Beam currently uses VLP16_hires_db.yaml \n"
+    	<< "----------------------------------------------------------\n";
     return -1;
-  } else {
+	} else {
 		std::string bag_file_path;
 		std::string calibration_file;
 		bag_file_path = argv[1];
 		calibration_file = argv[2];
 
     unpack_velodyne_scans::UnpackVelodyneScans unpack_velodyne_scans(
-			bag_file_path, calibration_file);
+		bag_file_path, calibration_file);
   }
   return 0;
 }
