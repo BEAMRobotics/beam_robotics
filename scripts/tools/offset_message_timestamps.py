4d42096a56a263d9b6849ccd13801971b940b9d8
viky397 <veronica.chatham2997@gmail.com>
2021-03-02 11:24:35 -0500

    changed to offset script to include all topics

--- scripts/tools/offset_message_timestamps.py
+++ scripts/tools/offset_message_timestamps.py
@@ -12,8 +12,11 @@ def offset_message_timestamps(bag_in, topics, offset, postfix):
     time_offset = rospy.Duration.from_sec(offset)
 
     for topic, msg, t in rosbag.Bag(bag_in).read_messages():
-        bag_out.write(topic, msg, t)
-        if topic in topics:
+        if any(fnmatchcase(topic, pattern) for pattern in topics):
+            if not topic in matchedtopics:
+                matchedtopics.append(topic)
+                if (args.verbose):
+                    print("Including matched topic '%s'" % topic)
             if topic == "/tf":
                 for transform in msg.transforms:
                     transform.header.stamp += time_offset
@@ -22,7 +25,7 @@ def offset_message_timestamps(bag_in, topics, offset, postfix):
             else:
                 raise RuntimeError('%s does not contain a header' % topic)
 
-            bag_out.write(topic + '_' + postfix, msg, t + time_offset)
+            bag_out.write(topic, msg, t + time_offset)
 
     bag_out.close()
 
@@ -32,14 +35,16 @@ if __name__ == "__main__":
                     'new bag file is written containing all of the original topics in the input '
                     'bag, with the addition of offset topics demarcated with a postfix.')
     parser.add_argument('--bag_in', required=True, nargs=1, help='input bag file')
-    parser.add_argument('--topics', required=True, nargs='+', help='topics to offset message timestamps')
+    parser.add_argument('--topics', required=True, default="*", help='topics to offset message timestamps, string interpreted as a list of topics (wildcards \'*\' and \'?\' allowed) to include in the merged bag file')
     parser.add_argument('--offset', required=True, nargs=1, type=float, help='time offset in seconds')
     parser.add_argument('--postfix', required=False, nargs=1, default='offset', 
                         help='postfix to demarcate output bag file and time-offset topics')
     args = parser.parse_args()
+    
+    topics = args.topics.split(' ')
+
     try:
-        offset_message_timestamps(args.bag_in[0], args.topics, args.offset[0], args.postfix)
+        offset_message_timestamps(args.bag_in[0], topics, args.offset[0], args.postfix)
     except Exception, e:
         import traceback
         traceback.print_exc()
-
