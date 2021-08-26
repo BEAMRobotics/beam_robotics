99fa8b578b8aaad659e9835ba9773bd67fe5bffd
alicejl <alicej.liang@gmail.com>
2021-08-26 11:12:32 -0400

    implemented flags to set models

--- inspection_tools/inspection/src/LabelImages.cpp
+++ inspection_tools/inspection/src/LabelImages.cpp
@@ -12,39 +12,81 @@ DEFINE_validator(images, &beam::gflags::ValidateDirMustExist);
 DEFINE_string(config, "", 
               "Full path to json config file. If none provided, it will use "
               "the file inspection/config/ImageLablerConfig.json");
+DEFINE_bool(crack, true, "Segment cracks from images");
+DEFINE_bool(delam, true, "Segment delaminations from images");
+DEFINE_bool(corrosion, true, "Segment corrosions from images");
+DEFINE_bool(spall, true, "Segment spalls from images");
+DEFINE_bool(vis, false, "Visualize segmentation results");
 
 
 int main(int argc, char* argv[]){
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   
+  //get current path
   std::string config_path;
   std::string file_name = "LabelImages.cpp";
   std::string curr_path = __FILE__;
   curr_path.erase(curr_path.end() - file_name.length(), curr_path.end());
 
 
+  //load config path
   if (FLAGS_config.empty()){
     config_path = inspection::utils::GetConfigFilePath("ImageLabelerConfig.json");
   }else{
     config_path = FLAGS_config;
   }
-  
-  printf("\n\n");
-  printf((curr_path + "lib/ImageLabeler.py").c_str());
-  printf("\n");
-  printf("/home/alice/projects/beam_robotics/inspection_tools/inspection/src/lib/ImageLabeler.py\n\n");
 
-  if ("/home/alice/projects/beam_robotics/inspection_tools/inspection/src/lib/ImageLabeler.py" == (curr_path + "lib/ImageLabeler.py").c_str()){
-     printf("why??? is it not working then!!!");
-}
+  //count number of model flags
+  int model_flags = 0;
+  if (!FLAGS_crack){
+    model_flags = model_flags + 1;
+  }
+  if (!FLAGS_delam){
+    model_flags = model_flags + 1;
+  }
+  if (!FLAGS_corrosion){
+    model_flags = model_flags + 1;
+  }
+  if (!FLAGS_spall){
+    model_flags = model_flags + 1;
+  }
+  if (FLAGS_vis){
+    model_flags = model_flags + 1;
+  }
 
   FILE* fp;
-  int argc_py = 3;
-  const char* argv_py[3]; 
-  argv_py[0] = curr_path + "lib/ImageLabeler.py";
-  argv_py[1] = const_cast<char*>(FLAGS_images.c_str());
-  argv_py[2] = const_cast<char*>(config_path.c_str());
+  int argc_py = 3 + model_flags;
+  const char* argv_py[argc_py]; 
 
+  //reqired flags
+  std::string py_ex_path = curr_path + "lib/ImageLabeler.py";
+  argv_py[0] = static_cast<const char*>(py_ex_path.c_str());
+  argv_py[1] = static_cast<const char*>(FLAGS_images.c_str());
+  argv_py[2] = static_cast<const char*>(config_path.c_str());
+
+  //optional flags
+  int idx = 3;
+  if (!FLAGS_crack){
+    argv_py[idx] = static_cast<const char*>("--crack");
+    idx = idx + 1;
+  }
+  if (!FLAGS_delam){
+    argv_py[idx] = static_cast<const char*>("--delam");
+    idx = idx + 1;
+  }
+  if (!FLAGS_corrosion){
+    argv_py[idx] = static_cast<const char*>("--corrosion");
+    idx = idx + 1;
+  }
+  if (!FLAGS_spall){
+    argv_py[idx] = static_cast<const char*>("--spall");
+    idx = idx +1;
+  }
+  if (FLAGS_vis){
+    argv_py[idx] = static_cast<const char*>("--vis");
+  }
+
+  //run python code
   Py_Initialize();
   PySys_SetArgvEx(argc_py, const_cast<char**>(argv_py), 1);
 
--- inspection_tools/inspection/src/lib/ImageLabeler.py
+++ inspection_tools/inspection/src/lib/ImageLabeler.py
@@ -4,16 +4,28 @@ from PIL import Image
 import os
 import json
 import numpy as np
+import argparse
 
 from torchvision.transforms import transforms as T
 from crack_segmentation_model import pretrained_mask_rcnn
 import sys
 
 
-# load models
+'''import tensorflow as tf
+from keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator
+from model import Deeplabv3
+from model import relu6, BilinearUpsampling
+import keras
+import keras.backend as K
+from deep_defect_functions import *'''
 
-def main(data_path, config_path):
-    print(config_path)
+# 
+
+
+# load models
+def main(data_path, config_path, crack, delam, corrosion, spall, visualize):
+    print("\n")
     if not config_path.endswith(".json"):
         print("ERROR: Config file needs to be .json")
         print("Config File:", config_path)
@@ -24,23 +36,68 @@ def main(data_path, config_path):
         print("Config File Path Given:", config_path)
         return
 
+    # load config file
     with open(config_path) as config_json:
         config_dict = json.load(config_json)
-        crack_model_path = config_dict["crack_model_path"]
-        num_classes = config_dict["num_classes"]  # crack and background classes
-        crack_confidence_threshold = config_dict["crack_confidence_threshold"]
-        crack_seg_threshold = config_dict["crack_seg_threshold"]
-    crack_model = pretrained_mask_rcnn(num_classes)
+        if crack:
+            #print("loading crack model")
+            crack_model_path = config_dict["crack_model_path"]
+            if not os.path.isfile(crack_model_path):
+                print("ERROR: Crack model file does not exist. Crack segmentation will not be completed")
+                crack = False
+            num_classes = config_dict["crack_num_classes"]  # crack and background classes
+            crack_confidence_threshold = config_dict["crack_confidence_threshold"]
+            crack_seg_threshold = config_dict["crack_seg_threshold"]
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    if torch.cuda.is_available():
-        crack_model.cuda()
+        if delam:
+            delam_model_path = config_dict["delam_model_path"]
+            if not os.path.isfile(delam_model_path):
+                print("ERROR: Delam model file does not exist. Delam segmentation will not be completed")
+                delam = False
+        else:
+            print("NO Delam")
 
-    crack_checkpoint = torch.load(crack_model_path, map_location=device)
-    crack_model.load_state_dict(crack_checkpoint["model"])
-    crack_model.eval()
+        if corrosion:
+            corrosion_model_path = config_dict["corrosion_model_path"]
+            if not os.path.isfile(corrosion_model_path):
+                print("ERROR: Corrosion model file does not exist. Corrosion segmentation will not be completed")
+                corrosion = False
 
-    # spall model
+        if spall:
+            spall_model_path = config_dict["spall_model_path"]
+            if not os.path.isfile(spall_model_path):
+                print("ERROR: Spall model file does not exist. Spall segmentation will not be completed")
+                spall = False
+
+    # load models
+    if crack:
+        crack_model = pretrained_mask_rcnn(num_classes)
+
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        if torch.cuda.is_available():
+            crack_model.cuda()
+        try:
+            crack_checkpoint = torch.load(crack_model_path, map_location=device)
+        except:
+            print("ERROR: Cannot load crack model file. Crack segmentation will not be completed")       
+        crack_model.load_state_dict(crack_checkpoint["model"])
+        crack_model.eval()
+
+    if delam: 
+        delam_model = load_model(delam_model_path,
+                           custom_objects={'relu6':relu6,
+                                           'BilinearUpsampling':BilinearUpsampling,
+                                           'perDelam':perDelam,
+                                           'predDelam':predDelam,
+                                           'realDelam':realDelam,
+                                           'iou_loss':iou_loss})
+
+    if corrosion:
+        print("load corrosion model")
+
+    if spall:
+        print("load spall model")
+
 
     # get cameras and iterate through them
     with open(data_path + "CamerasList.json") as camera_json:
@@ -62,40 +119,80 @@ def main(data_path, config_path):
                 if width > 3000 or height > 2000:
                     new_size = (int(width/2), int(height/2))
                     img = img.resize(new_size)
-                img_tensor = [T.ToTensor()(img)]
 
-                masks = []
-                with torch.no_grad():
-                    output = crack_model(img_tensor)[0]
-                    for i in range(len(output["scores"])):
-                        if output["scores"][i] > crack_confidence_threshold:
-                            mask = output["masks"][i]
-                            mask = np.where(mask > crack_seg_threshold, 1, 0).astype(np.uint8)
-                            masks.append(mask)
-                            mask *= 255
+                if crack:
+                    crack_masks = []
+                    img_tensor = [T.ToTensor()(img)]
 
-                            if "mask_rcnn" not in mask_methods:
-                                mask_methods.append("mask_rcnn")
+                    with torch.no_grad():
+                        output = crack_model(img_tensor)[0]
+                        for i in range(len(output["scores"])):
+                            if output["scores"][i] > crack_confidence_threshold:
+                                mask = output["masks"][i]
+                                mask = np.where(mask > crack_seg_threshold, 1, 0).astype(np.uint8)
+                                crack_masks.append(mask)
 
+                                if "mask_rcnn_crack" not in mask_methods:
+                                    mask_methods.append("mask_rcnn_crack")
+
+                    crack_mask = (np.logical_or.reduce(crack_masks)).astype(np.uint8)
+                    crack_mask = np.transpose(crack_mask[0], (1, 0))
                 # spalling et al detection here
+                bgr_base = np.zeros((img.size[1], img.size[0]))
+                
+                # delam model used for ir images only
+                # if delam:
+                #     bgr_base[np.where(delam_mask ==1)] = 2
+                if corrosion:
+                    bgr_base[np.where(corrosion_mask == 1)] = 3
+                if crack:
+		    idx = np.where(crack_mask != 0)
+                    bgr_base[idx[1], idx[0]] = 1
+                if spall:
+                    bgr_base[np.where(spall_mask == 1)] = 4
 
-                if len(masks) > 0:
-                    mask = (np.logical_or.reduce(masks) * 255).astype(np.uint8)
-                    mask_img = Image.fromarray(np.transpose(np.stack([mask, mask, mask], axis=1)[0], [1, 2, 0]))
-                    mask_img = mask_img.resize((width, height))
-                    mask_img.save(data_path + camera + "/" + img_name + "/BGRMask.jpg")
-                else:
-                    mask_img = Image.new('RGB', (width, height))
-                    mask_img.save(data_path + camera + "/" + img_name + "/BGRMask.jpg")
+                mask_img = Image.fromarray(np.transpose(np.stack([bgr_base, bgr_base, bgr_base], axis=1), [0, 2, 1]).astype(np.uint8))
+                mask_img = mask_img.resize((width, height))
+                mask_img.save(data_path + camera + "/" + img_name + "/BGRMask.jpg")
+
+                if visualize:
+                    bgr_base *= 50#np.where(bgr_base != 0, 255, 0)
+                    mask_img_visualize = Image.fromarray(np.transpose(np.stack([bgr_base, bgr_base, bgr_base], axis=1), [0, 2, 1]).astype(np.uint8))
+                    mask_img_visualize = mask_img_visualize.resize((width, height))
+                    mask_img_visualize.save(data_path + camera + "/" + img_name + "/BGRMask_visualize.jpg")
 
                 img_info["bgr_mask_method"] = ", ".join(mask_methods)
                 img_info["is_bgr_mask_set"] = True
+
+            if img_info["is_ir_image_set"]:
+                if delam:
+                    img = Image.open(data_path + camera + "/" + img_name + "/IRImage.jpg")
+                    img_scale = customRescale(img)
+                    res = delam_model.predict(np.expand_dims(img_scale,0), batch_size=1)
+                    labels = np.argmax(res.squeeze(),-1)
+                    mask = Image.fromarray(labels)
+                    mask.save(data_path + camera + "/" + img_name + "/IRMask.jpg")
+
+                    if visualize:
+                        mask_visualize = Image.fromarray(labels*255)
+                        mask_visualize.save(data_path + camera + "/" + img_name + "/IRMask_visualize.jpg")
+
+                    img_info["ir_mask_method"] = "Deeplabv3_delam"
+                    img_info["is_ir_mask_set"] = True
 
             with open(data_path + camera + "/" + img_name + "/ImageBridgeInfo.json", "w") as img_json:
                 json.dump(img_info, img_json)
 
 
 if __name__ == "__main__":
-    data_path = sys.argv[1]
-    config_file_path = sys.argv[2]
-    main(data_path, config_file_path)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_path')
+    parser.add_argument('config', type=str)
+    parser.add_argument('--crack', action='store_false')
+    parser.add_argument('--delam', action='store_false')
+    parser.add_argument('--corrosion', action='store_false')
+    parser.add_argument('--spall', action='store_false')
+    parser.add_argument('--vis', action='store_true')
+
+    args = parser.parse_args()
+    main(args.data_path, args.config, args.crack, args.delam, args.corrosion, args.spall, args.vis)
