b39c6846f4f2f167b6ea05b1de6c59715490cb27
Gabe <earlegabriel1@gmail.com>
2021-09-07 20:18:10 -0400

    use header timestamps for non sync messages

--- scripts/tools/restamp.py
+++ scripts/tools/restamp.py
@@ -13,22 +13,27 @@ def restamp(bag, outbag, data_topics, time_topics):
     for i, topic in enumerate(time_topics):
         stamps[i] = topic_to_array(bag, topic)
 
-    for data_topic, msg, t in bag.read_messages(data_topics):
-        try:
-            time_msg = stamps[data_topics.index(data_topic)].pop(0)
-        except:
-            print("Can't stamp " + data_topic + " seq " + str(msg.header.seq) + ": ran out of timestamps.")
-            continue
-       
-        msg.header.stamp = time_msg.time_ref
-        outbag.write(data_topic, msg, time_msg.time_ref)
+    for topic, msg, t in bag.read_messages():
+        if topic in data_topics:
+            try:
+                time_msg = stamps[data_topics.index(topic)].pop(0)
+            except:
+                print("Can't stamp " + topic + " seq " + str(msg.header.seq) + ": ran out of timestamps.")
+                continue
+        
+            msg.header.stamp = time_msg.time_ref
+            outbag.write(topic, msg, time_msg.time_ref)
+
+        # preserve unspecified topics
+        elif topic not in time_topics:
+            outbag.write(topic, msg, msg.header.stamp)
 
     return outbag
     
         
 def main(args):
     parser = argparse.ArgumentParser(
-        description='Plot TimeReference msgs to validate synchronization.')
+        description='Restamp and reserialize data topics with corresponding TimeReference topics.  Unspecified topics are preserved.')
     parser.add_argument('-b', '--bag', help='input bag file', required=True)
     parser.add_argument(
         '-d', '--data_topics', nargs='+', help='whitespace separated list of topics', required=True)
