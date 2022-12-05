5c198cf056dbb50f3cb961ce029fa32ba46f06a5
nickcharron <nicholas.c.charron@gmail.com>
2022-12-04 23:42:28 -0800

    fix seg fault in image extration

--- inspection_tools/inspection/src/lib/ImageExtractor.cpp
+++ inspection_tools/inspection/src/lib/ImageExtractor.cpp
@@ -180,26 +180,25 @@ void ImageExtractor::OutputImages() {
     BEAM_INFO("Saving images for Camera {}.", cam_count);
     std::string camera_dir =
         save_directory_ + "/camera" + std::to_string(cam_count);
-    
+
     boost::filesystem::create_directories(camera_dir);
     camera_list_.push_back("camera" + std::to_string(cam_count));
     image_object_list_.clear();
-    
+
     // iterate over all poses for this camera
     for (uint32_t image_count = 0;
          image_count < image_time_stamps_[cam_count].size(); image_count++) {
       ros::Time image_time = image_time_stamps_[cam_count][image_count];
       cv::Mat image =
           GetImageFromBag(image_time, cam_count, (image_count == 0));
-      
+
       if (image_container_type_ == "ImageBridge") {
-        
         beam_containers::ImageBridge image_container;
         std::string image_container_dir = camera_dir + "/" +
                                           image_container_type_ +
                                           std::to_string(image_count);
         boost::filesystem::create_directories(image_container_dir);
-        
+
         if (is_ir_camera_[cam_count]) {
           image_container.SetIRImage(image);
           image_container.SetIRIsDistorted(
@@ -211,7 +210,7 @@ void ImageExtractor::OutputImages() {
               are_output_images_distorted_[cam_count]);
           image_container.SetBGRFrameId(frame_ids_[cam_count]);
         }
-        
+
         beam::TimePoint image_timepoint = beam::RosTimeToChrono(image_time);
         image_container.SetTimePoint(image_timepoint);
         image_container.SetImageSeq(image_count);
@@ -236,12 +235,12 @@ void ImageExtractor::OutputImages() {
             "header is included in ImageExtractor.h"};
       }
     }
-    
+
     if (image_object_list_.size() > 0) {
       OutputJSONList(camera_dir + "/ImagesList.json", image_object_list_);
     }
   }
-  
+
   if (camera_list_.size() > 0) {
     OutputJSONList(save_directory_ + "/CamerasList.json", camera_list_);
   }
@@ -277,17 +276,21 @@ cv::Mat ImageExtractor::GetImageFromBag(ros::Time& image_time, int cam_number,
     image_time = img_msg->header.stamp;
     last_image_time_ = img_msg->header.stamp;
     if (first_image) { frame_ids_.push_back(img_msg->header.frame_id); }
+    
+    cv::Mat image_raw;
     if (img_msg->encoding == sensor_msgs::image_encodings::BGR8) {
-      cv::Mat image = beam_cv::OpenCVConversions::RosImgToMat(*img_msg);
-      return ApplyImageTransforms(image, cam_number);
+      image_raw = beam_cv::OpenCVConversions::RosImgToMat(*img_msg);
     } else if (img_msg->encoding == sensor_msgs::image_encodings::RGB8) {
       cv::Mat image_RGB = beam_cv::OpenCVConversions::RosImgToMat(*img_msg);
-      cv::Mat image_BGR;
-      cv::cvtColor(image_RGB, image_BGR, cv::COLOR_RGB2BGR);
-      return ApplyImageTransforms(image_BGR, cam_number);
+      cv::cvtColor(image_RGB, image_raw, cv::COLOR_RGB2BGR);
     } else {
-      cv::Mat image = beam_cv::OpenCVConversions::RosImgToMat(*img_msg);
-      return ApplyImageTransforms(image, cam_number);
+      image_raw = beam_cv::OpenCVConversions::RosImgToMat(*img_msg);
+    }
+
+    if (image_transforms_[cam_number].empty()) {
+      return image_raw.clone();
+    } else {
+      return ApplyImageTransforms(image_raw, cam_number);
     }
   }
 }
@@ -297,7 +300,7 @@ cv::Mat ImageExtractor::ApplyImageTransforms(const cv::Mat& image,
   std::vector<ImageTransform> transforms = image_transforms_[cam_number];
 
   // avoid useless copying if no transforms are specified
-  if (transforms.size() == 0) { return image; }
+  if (transforms.size() == 0) { return image.clone(); }
 
   cv::Mat output_image = image.clone();
   for (ImageTransform transform : transforms) {
@@ -315,7 +318,7 @@ cv::Mat ImageExtractor::ApplyImageTransforms(const cv::Mat& image,
                 transform.type);
     }
   }
-  return output_image;
+  return output_image.clone();
 }
 
 cv::Mat
