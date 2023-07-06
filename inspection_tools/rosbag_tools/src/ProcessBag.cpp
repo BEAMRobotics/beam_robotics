7b98fab449cf9312a9a9749ac653c07e503cbff3
Jake McLaughlin <jake.mclaughlin98@gmail.com>
2023-07-06 18:12:04 -0400

    Fix process bag (#113)
    * Add rectification options to process bag
    
    * Update libbeam
    
    * Update libbeam
    
    * update libbeam

--- inspection_tools/rosbag_tools/src/ProcessBag.cpp
+++ inspection_tools/rosbag_tools/src/ProcessBag.cpp
@@ -3,6 +3,8 @@
 
 #include <gflags/gflags.h>
 
+#include <beam_calibration/CameraModel.h>
+#include <beam_calibration/ConvertCameraModel.h>
 #include <beam_cv/OpenCVConversions.h>
 #include <beam_utils/filesystem.h>
 #include <beam_utils/gflags.h>
@@ -11,12 +13,14 @@ DEFINE_string(input, "", "Full path to input bag file (Required)");
 DEFINE_validator(input, &beam::gflags::ValidateBagFileMustExist);
 DEFINE_string(output, "", "Full path to output bag file (Required)");
 DEFINE_validator(output, &beam::gflags::ValidateDirMustExist);
-DEFINE_bool(unpack_scans, true,
+DEFINE_bool(unpack_scans, false,
             "Set to true if you want to unpack velodyne scans. New topic will "
             "be the scan topic post-fixed with _unpacked");
 DEFINE_bool(debayer_images, true,
             "Set to true if you want to debayer images. Topics will be "
             "post-fixed with _debayered");
+DEFINE_bool(rectify_images, true,
+            "Set to true if you want to rectify images.");
 DEFINE_double(
     resize_multiplier, 1,
     "Setting to a value in range (0,1) will resize images using this "
@@ -27,36 +31,30 @@ DEFINE_double(
 DEFINE_string(lidar_model, "VLP16",
               "Only needed if unpack_scans is set to true. Options: VLP16, "
               "32C, 32E, VLS128. (Optional)");
+DEFINE_string(camera_model_path, "",
+              "Path to camera model (Required if rectified = true)");
 
-sensor_msgs::Image ProcessImage(const sensor_msgs::Image& msg,
-                                const double& resize_multiplier) {
+sensor_msgs::Image ProcessImage(
+    const sensor_msgs::Image& msg, const double& resize_multiplier,
+    std::shared_ptr<beam_calibration::ConvertCameraModel>& converter) {
   using namespace beam_cv;
   using namespace OpenCVConversions;
 
-  // if multiplier is 1, then we only need to debayer
-  if (resize_multiplier == 1) {
-    if (msg.encoding.find("bayer") != std::string::npos) {
-      cv::Mat mat = RosImgToMat(msg);
-
-      // get new image encoding
-      auto iter = bayer_decoding_map.find(msg.encoding);
-      if (iter == bayer_decoding_map.end()) {
-        BEAM_CRITICAL("Bayered image encoding ({}) not yet supported",
-                      msg.encoding);
-        throw std::runtime_error{"image encoding not supported"};
-      }
-      return MatToRosImg(mat, msg.header, iter->second);
-    }
-    return msg;
-  }
-
   // else, convert to cv::Mat which will automatically do the encoding
   cv::Mat mat = RosImgToMat(msg);
-  cv::Mat mat_resized;
-  cv::resize(
-      mat, mat_resized,
-      cv::Size(resize_multiplier * mat.cols, resize_multiplier * mat.rows),
-      cv::INTER_LINEAR);
+
+  // rectify image
+  if (FLAGS_rectify_images) { mat = converter->ConvertImage<cv::Vec3b>(mat); }
+
+  cv::Mat mat_resized = mat.clone();
+
+  if (resize_multiplier != 1) {
+    cv::resize(
+        mat, mat_resized,
+        cv::Size(resize_multiplier * mat.cols, resize_multiplier * mat.rows),
+        cv::INTER_LINEAR);
+  }
+
   std::string encoding = msg.encoding;
   if (msg.encoding.find("bayer") != std::string::npos) {
     auto iter = bayer_decoding_map.find(encoding);
@@ -87,13 +85,24 @@ int main(int argc, char* argv[]) {
   RosBagReader reader(FLAGS_input);
   RosBagWriter writer(FLAGS_output);
 
+  std::shared_ptr<beam_calibration::CameraModel> cam_model;
+  std::shared_ptr<beam_calibration::ConvertCameraModel> converter;
+  if (FLAGS_rectify_images) {
+    cam_model = beam_calibration::CameraModel::Create(FLAGS_camera_model_path);
+    auto height = cam_model->GetHeight();
+    auto width = cam_model->GetWidth();
+    Eigen::Vector2i size(height, width);
+    std::cout << size << std::endl;
+    converter = std::make_shared<beam_calibration::ConvertCameraModel>(
+        cam_model, size, size);
+  }
+
   while (true) {
     rosbag::View::iterator iter;
     if (!reader.GetNextMsg(iter)) { break; }
 
     if (FLAGS_unpack_scans) {
-      velodyne_msgs::VelodyneScan::ConstPtr maybe_lidar_msg =
-          iter->instantiate<velodyne_msgs::VelodyneScan>();
+      auto maybe_lidar_msg = iter->instantiate<velodyne_msgs::VelodyneScan>();
       if (maybe_lidar_msg) {
         sensor_msgs::PointCloud2 cloud =
             velodyne_tools.UnpackScan(maybe_lidar_msg);
@@ -105,11 +114,10 @@ int main(int argc, char* argv[]) {
     if (FLAGS_debayer_images || FLAGS_resize_multiplier != 1) {
       std::string postfix =
           FLAGS_resize_multiplier != 1 ? "_resized" : "_debayered";
-      boost::shared_ptr<sensor_msgs::Image> maybe_image_msg =
-          iter->instantiate<sensor_msgs::Image>();
+      auto maybe_image_msg = iter->instantiate<sensor_msgs::Image>();
       if (maybe_image_msg != nullptr) {
         sensor_msgs::Image new_msg =
-            ProcessImage(*maybe_image_msg, FLAGS_resize_multiplier);
+            ProcessImage(*maybe_image_msg, FLAGS_resize_multiplier, converter);
         writer.AddMsg(iter->getTopic() + postfix, iter->getTime(), new_msg);
         continue;
       }
